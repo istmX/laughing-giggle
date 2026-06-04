55883ef428b6a9aba9c82f1059747d96320b0fc3
Aryan <roaringmoon07@gmail.com>
2026-06-04 06:07:22 +0000

    feat: add user model and schema for authentication

---
+++ backend/src/features/auth/auth.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+    id: {
+        type:mongoose.Schema.Types.ObjectId,
+        required: true,
+        unique: true,
+    },
+    name:{
+        type:String,
+        required: true
+    },
+    email:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+})
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
