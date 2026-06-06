6d9fa5f0454a1eb26f879ed3f22bc8995ff1648e
Aryan <roaringmoon07@gmail.com>
2026-06-06 04:54:51 +0000

    feat: created idea schema

---
+++ backend/src/features/ideas/idea.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const ideaSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    prompt: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 500,
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "generated"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Idea = mongoose.model("Idea", ideaSchema);
+
+export default Idea;
