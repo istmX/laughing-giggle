4d3bbbfb714746e481129ccd2bef073cf3a3e23d
Aryan <roaringmoon07@gmail.com>
2026-06-06 03:33:05 +0000

    feat:updated some logic and fixed errors in projects

--- backend/src/features/projects/project.model.js
+++ backend/src/features/projects/project.model.js
@@ -1,40 +1,39 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const projectSchema = new mongoose.Schema({
-    owner:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+const projectSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true
     },
 
-    project_id:{
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        unique: true
-    },
-    project_title:{
-        type: String,
-        required: true
+    project_title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 100
     },
 
-    project_description:{
-        type: String,
-        required: true
+    project_description: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 2000
     },
 
-    project_status:{
-        type: String,
-        enum: ['active', 'completed', 'archived'],
-        default: 'active'
-    },
-
- 
-
-},{
+    project_status: {
+      type: String,
+      enum: ["active", "completed", "archived"],
+      default: "active"
+    }
+  },
+  {
     timestamps: true
-})
+  }
+);
 
-
-const Project = mongoose.model("Projects", projectSchema);
+const Project = mongoose.model("Project", projectSchema);
 
 export default Project;
